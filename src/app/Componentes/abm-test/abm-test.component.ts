b7f203f9dd0c23341f9143680c06a5f500c4db83
Gianfortone Pablo <pgianfortone@sspv.com.ar>
2023-12-20 02:22:06 -0300

    no message

--- src/app/Componentes/abm-test/abm-test.component.ts
+++ src/app/Componentes/abm-test/abm-test.component.ts
@@ -2,6 +2,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+
 @Component({
   selector: 'app-abm-test',
   templateUrl: './abm-test.component.html',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './Componentes/login/login.component';
 import { CookieService } from 'ngx-cookie-service';
-import { AuthInterceptor } from './Interceptors/AuthInterceptor';
+import { AuthInterceptor } from './AuthInterceptor';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
